eb1a168fa491ea974d1ff6e19eec34553e0d4435
Abdul Rauf <armujahid@live.com>
2015-06-30 23:57:01 +0500

    Artist images retrieval + custom array adapter for binding custom artist object

--- app/src/main/java/me/armujahid/spotifystreamer/SearchActivityFragment.java
+++ app/src/main/java/me/armujahid/spotifystreamer/SearchActivityFragment.java
@@ -1,9 +1,10 @@
 package me.armujahid.spotifystreamer;
-
+import android.app.Activity;
 import android.content.Context;
 import android.os.AsyncTask;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -27,7 +28,8 @@ import kaaes.spotify.webapi.android.models.ArtistsPager;
  */
 public class SearchActivityFragment extends Fragment {
 
-    private ArrayAdapter<String> mArtistAdapter;
+    final String LOG_TAG = Activity.class.getName();
+    private customAdapter mCustomAdapter;
 
 
     public SearchActivityFragment() {
@@ -51,46 +53,78 @@ public class SearchActivityFragment extends Fragment {
 //                output.setText(input.getText());
             }
         });
-        mArtistAdapter =
-                new ArrayAdapter<String>(
+        mCustomAdapter =
+                new customAdapter(
                         getActivity(), // The current context (this activity)
-                        R.layout.list_item_artist, // The name of the layout ID.
-                        R.id.list_item_artist_textview, // The ID of the textview to populate.
-                        new ArrayList<String>());
+                        R.layout.linearlayout_artist, // The name of the layout ID.
+                        new ArrayList<customArtist>(),
+                        R.id.list_item_artist_imageview,
+                        R.id.list_item_artist_textview
+                );
         // Get a reference to the ListView, and attach this adapter to it.
         ListView listView = (ListView) rootView.findViewById(R.id.listview_artist);
-        listView.setAdapter(mArtistAdapter);
+        listView.setAdapter(mCustomAdapter);
         return rootView;
 
     }
 
-    public class FetchArtistTask extends AsyncTask<String, Void, String[]> {
+    public class FetchArtistTask extends AsyncTask<String, Void, customArtist[]> {
 
         @Override
-        protected String[] doInBackground(String... params) {
+        protected customArtist[] doInBackground(String... params) {
             if (params.length == 0) {
+                Log.e(LOG_TAG,"params length is 0");
                 return null;
             }
             SpotifyApi api = new SpotifyApi();
             SpotifyService spotify = api.getService();
             ArtistsPager results = spotify.searchArtists(params[0]);
-            String[] result = new String[results.artists.items.size()];
-            int i = 0;
-            for(Artist artist : results.artists.items)
+            if (results == null)
             {
-                result[i] = artist.name.toString();
-                i++;
+                Log.e(LOG_TAG,"Artistpager is null");
+                return null;
+            }
+            int size = results.artists.items.size();
+            if (size == 0)
+            {
+                Log.e(LOG_TAG,"Artist items size is "+size);
+                return null;
+            }
+            customArtist[] customArtists = new customArtist[size];
+
+
+            int i = 0;
+            try
+            {
+                for (Artist artist : results.artists.items) {
+                    customArtists[i] = new customArtist();
+                    customArtists[i].name = artist.name;
+                    int imagelistsize = artist.images.size();
+                    if (imagelistsize != 0)
+                    {
+                        customArtists[i].image = artist.images.get(imagelistsize-1); //almost 64x64 image
+                    }
+                    customArtists[i].id = artist.id;
+
+                    i++;
+                }
+            }
+            catch (Exception ex)
+            {
+                Log.e(LOG_TAG,ex.getMessage());
+                Log.e(LOG_TAG,"i = "+i);
+                return null;
             }
 
-            return result;
+            return customArtists;
         }
 
         @Override
-        protected void onPostExecute(String[] result) {
+        protected void onPostExecute(customArtist[] result) {
             if (result != null) {
-                mArtistAdapter.clear();
-                for(String artist : result) {
-                    mArtistAdapter.add(artist);
+                mCustomAdapter.clear();
+                for(customArtist artist : result) {
+                    mCustomAdapter.add(artist);
                 }
                 // New data is back from the server.  Hooray!
             }
@@ -106,5 +140,3 @@ public class SearchActivityFragment extends Fragment {
         }
     }
 }
-
-
---
+++ app/src/main/java/me/armujahid/spotifystreamer/customArtist.java
@@ -0,0 +1,15 @@
+package me.armujahid.spotifystreamer;
+
+import kaaes.spotify.webapi.android.models.Image;
+
+/**
+ * Created by Abdul-Rauf on 28/06/2015.
+ */
+public class customArtist {
+    public Image image;
+    public String name;
+    public String id;
+    public customArtist()
+    {
+    }
+}
